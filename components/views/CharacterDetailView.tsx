853baeecd8ed9055e75788cf81ed735b338ea0f4
Bruno-mrBLONDE <123082731+Leemonztuff@users.noreply.github.com>
2026-05-13 00:05:39 +0000

    fix: remove stale CharacterDetailView re-export (deleted dependency)

--- components/views/CharacterDetailView.tsx
+++
@@ -1,7 +0,0 @@
-// Reexport styled component to replace previous CharacterDetailView
-"use client";
-import CharacterDetailStyled from './CharacterDetailStyled';
-
-export default function CharacterDetailView({ characterId }: { characterId: string }) {
-  return <CharacterDetailStyled characterId={characterId} />;
-}
